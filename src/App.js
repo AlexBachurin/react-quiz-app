c68484e7ad45b7122fa1525b5fb3763e1a7595ca
Alex <alexbachurin91@gmail.com>
2022-01-12 18:20:47 +0300

    display question

--- src/App.js
+++ src/App.js
@@ -5,16 +5,37 @@ import Loading from "./components/Loading";
 import { useGlobalContext } from './context'
 
 function App() {
-  const { loading, waiting, questions, correct } = useGlobalContext();
+  const { loading, waiting, questions, correct, index } = useGlobalContext();
   if (waiting) {
     return <SetupForm />
   }
   if (loading) {
     return <Loading />
   }
+  //get question by current index
+  const currentQuestion = questions[index];
+  console.log(currentQuestion);
+  //destructure and get array of answers from data
+  const { question, correct_answer, incorrect_answers } = currentQuestion;
+  const answers = [...correct_answer, ...incorrect_answers];
   return (
     <main>
-      Quiz app
+      <section className="quiz">
+        <p className="correct-answers">correct answers {correct}/{index}</p>
+        <article className="container">
+          {/* use dangerouslyHtml since we get not strings in response but html */}
+          <h2 dangerouslySetInnerHTML={{ __html: question }} />
+          <div className="btn-container">
+            {answers.map((item, index) => {
+              return (
+                <button key={index} className="answer-btn" dangerouslySetInnerHTML={{ __html: item }} />
+
+              )
+            })}
+          </div>
+          <button className="next-question">next question</button>
+        </article>
+      </section>
     </main>
   );
 }
